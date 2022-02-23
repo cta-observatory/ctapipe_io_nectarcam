d23ec5d112641f63945004c48f2dcfc6ceb9b852
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-02-23 14:24:40 +0100

    Use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
+import os
 from setuptools import setup, find_packages
-from os import path
-
-# read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setup(
     name='ctapipe_io_nectarcam',
-    packages=find_packages(),
-    version='0.1',
+    packages=find_packages(exclude=["ctapipe_io_nectarcam._dev_version"]),
+    use_scm_version={"write_to": os.path.join("ctapipe_io_nectarcam", "_version.py")},
     description='ctapipe plugin for reading NectarCam files',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     install_requires=[
         'astropy',
         'ctapipe',
-        'protozfits @ https://github.com/cta-sst-1m/protozfitsreader/archive/v1.5.0.tar.gz',
+        'protozfits~=2.0',
+        'setuptools_scm',
     ],
-    tests_require=['pytest'],
-    setup_requires=['pytest_runner'],
     author='Franca Cassol',
     author_email='cassol@cppm.in2p3.fr',
     license='MIT',
