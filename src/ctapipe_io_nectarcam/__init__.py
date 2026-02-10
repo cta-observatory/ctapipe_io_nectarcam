c41ffdf04feef24925664d80edf2eddeaf3b42f8
Jean-Philippe Lenain <jlenain@in2p3.fr>
2026-02-10 22:02:25 +0100

    Dummy commit to trigger CI on GitHub after downtime on 2026-02-10

--- src/ctapipe_io_nectarcam/__init__.py
+++ src/ctapipe_io_nectarcam/__init__.py
@@ -412,6 +412,7 @@ def read_pulse_shapes():
     # https://gitlab.cta-observatory.org/cta-consortium/aswg/simulations/
     # simulation-model/simulation-model-description/-/blob/master/datFiles/
     # Pulse_template_nectarCam_17042020.dat
+
     infilename = (
         files("ctapipe_io_nectarcam")
         / "resources/Pulse_template_nectarCam_17042020.dat"
